876112a24ddb86f32698251d9a8d27a698600609
Mike Linz <mike.linz@stud.hs-merseburg.de>
2025-10-30 15:58:42 +0100

    Aufgabe 1: Basics.java mit Variablen erstellt

---
+++ Basics.java
@@ -0,0 +1,15 @@
+public class Basics
+{
+    public static void main(String[] args)
+    {
+        byte fensterOffenAnzahl = 2;
+        short anzahlLampen = 14;
+        int verbrauchHeuteWh = 3260;
+        long gesamtVerbrauchWh = 124875L;
+        float temperaturInnen = 21.5f;
+        double temperaturAussen = 16.73;
+        char modusSymbol = 'h';
+        boolean bewegungErkannt = true;
+        String raumName = "Wohnzimmer";
+    }
+}
