e75e2f136f30e23c2a649dad1f0754fe2e8f64a2
Mike Linz <mike.linz@stud.hs-merseburg.de>
2025-10-30 16:25:04 +0100

    Aufgabe 1.2: Temperaturdifferenz und Gesamtstromverbrauch

--- Basics.java
+++ Basics.java
@@ -2,6 +2,7 @@ public class Basics
 {
     public static void main(String[] args)
     {
+        //1.1
         byte fensterOffenAnzahl = 2;
         short anzahlLampen = 14;
         int verbrauchHeuteWh = 3260;
@@ -11,5 +12,24 @@ public class Basics
         char modusSymbol = 'h';
         boolean bewegungErkannt = true;
         String raumName = "Wohnzimmer";
+        System.out.println("Aufgabe 1.1:\nfensterOffenAnzahl = " + fensterOffenAnzahl + "\n" +
+            "anzahlLampen = " + anzahlLampen + "\n" +
+            "verbrauchHeuteWh = " + verbrauchHeuteWh + "\n" +
+            "gesamtVerbrauchWh = " + gesamtVerbrauchWh + "\n" + 
+            "temperaturInnen = " + temperaturInnen + "\n" +
+            "temperaturAussen = " + temperaturAussen + "\n" +
+            "modusSymbol = " + modusSymbol + "\n" +
+            "bewegungErkannt = " + bewegungErkannt + "\n" +
+            "raumName = " + raumName + "\n");
+
+        //1.2
+        //a)
+        double temperaturDifferenz = temperaturInnen - temperaturAussen;
+        System.out.println("Aufgabe 1.2.a)\ntemperaturDifferenz betraegt " + temperaturDifferenz + " Grad.\n");
+        //b)
+        gesamtVerbrauchWh += verbrauchHeuteWh;
+        System.out.println("Aufgabe 1.2.b)\nNeuer gesamtVerbrauchWh betraegt " + gesamtVerbrauchWh + " Wh.\n");
+
+
     }
 }
